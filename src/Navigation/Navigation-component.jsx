1a832f3328e26950ee9bd99296c3521e7da9be15
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-08-05 09:09:48 -0400

    [Navigation] Created Navigation Component

---
+++ src/Navigation/Navigation-component.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Navigation-styles.scss"
+const Navigation = () => {
+    return(
+        <div>
+            <nav>
+                <li className="nav-item"><a href="/" className="sign-out">Sign Out</a></li>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Navigation
