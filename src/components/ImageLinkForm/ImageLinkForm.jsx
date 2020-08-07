6f7b5cbd3886c38a7785e715c5f128afe4052858
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-08-07 10:56:29 -0400

    [ImageLinkForm] Updated ImageLinkForm Component

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,18 +1,17 @@
 import React from "react"
 import "./ImageLinkForm.scss"
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onChange, onClick}) => {
     return(
         <div className="image-link-form">
             <div>
-
-            <div className="form">
-            <p className="form-title">
-                This app can detect faces!
-            </p>
-                <input type="text" className="input" placeholder="Enter image link here"/>
-                <button className="primary">Detect</button>
-            </div>
-            </div>
+                <div className="form">
+                    <p className="form-title">
+                        This app can detect faces!
+                    </p>
+                    <input onChange={onChange} type="text" className="input" placeholder="Enter image link here"/>
+                    <button className="primary" onClick={onClick}>Detect</button>
+                </div>
+                </div>
 
         </div>
     )
