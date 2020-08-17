5ff7ef5ba49733ce52a32aa3f2f2b212f205d061
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-08-17 11:42:44 -0400

    [Register] Hooking up front and backend

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,14 +1,63 @@
 import React from "react"
 import "./SignIn-styles.scss"
+import { Component } from "react"
 
-const SignIn = ({onRouteChange}) => {
-    return(
-        <form className="signin">
-            <input type="text" placeholder="username"/>
-            <input type="text" placeholder="password"/>
-            <input type="submit" value="Sign In" onClick={()=> onRouteChange("home")} />
-        </form>
-    )
+class SignIn extends Component{ 
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            info: ""
+        }
+}
+
+    onEmailChange = (e) => {
+        this.setState({email: e.target.value})
+    }
+
+    onPasswordChange = (e) =>{
+        this.setState({password: e.target.value})
+    }
+
+    onSubmitSignIn = (event) =>{
+        event.preventDefault()
+        fetch("http://localhost:3000/signin",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(data === "success"){
+                this.props.onRouteChange("home")
+            }
+            else{
+                this.setState({info: <p>Wrong credentials, please try again</p>})
+                
+            }
+        })
+    }
+
+    render(){
+        const {onRouteChange} = this.props
+
+        return(
+            <form className="signin">
+                <input type="text" placeholder="username" onChange={this.onEmailChange}/>
+                <input type="text" placeholder="password" onChange={this.onPasswordChange}/>
+
+                <input type="submit" value="Sign In" onClick={this.onSubmitSignIn} />
+
+                <input type="submit" value="Register" onClick={()=> onRouteChange("register")} />
+                {this.state.info}
+            </form>
+        )
+    }
+
 }
 
 export default SignIn
